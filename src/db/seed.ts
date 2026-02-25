06db448c8257b82b319476f9c3098134a88cdac2
Wes Winder <weswinder@users.noreply.github.com>
2026-02-24 23:45:54 -0500

    Updated vite.config.ts

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({ path: '.env.local' });
 
 const supabaseUrl = process.env.VITE_SUPABASE_URL!;
 const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY!;
